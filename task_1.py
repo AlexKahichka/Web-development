666d32073f002dd6d8299ca1436b893aa05874f9
Alex.Kaha <alexprogithub@gmail.com>
2024-11-06 17:06:48 +0100

    Added comments about examples

--- task_1.py
+++ task_1.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, EmailStr, Field, field_validator, model_validator
-from typing import Any
+
 
 class Address(BaseModel):
     city: str = Field(..., min_length=2)
@@ -16,20 +16,20 @@ class User(BaseModel):
     @field_validator("name")
     def validate_name(cls, value: str) -> str:
         if not value.isalpha():
-            raise ValueError("Der Name darf nur Buchstaben enthalten.")
+            raise ValueError("Name can contain only letters.")
         return value
 
     @model_validator(mode="after")
     def validate_employment_age(self) -> "User":
         if self.age < 18 and self.is_employed:
-            raise ValueError("Beschäftigung ist erst ab 18 Jahren erlaubt.")
+            raise ValueError("Employment age cannot be less than 18 years.")
         return self
 
 def validate_json_string(json_string: str) -> str:
     user = User.model_validate_json(json_string)
     return user.model_dump_json()
 
-# Примеры JSON строк для проверки
+# Sample JSON strings
 valid_json = """
 { "name": "Alex",
     "age": 20,
@@ -56,15 +56,15 @@ invalid_json = """
 }
 """
 
-# Проверка успешного случая
+# Example of correct data
 valid_user = validate_json_string(valid_json)
 print(valid_user)
 
-print("#" * 100)
+print("*" * 100)
 
-# Проверка случая с ошибкой
+# Example of incorrect data
 try:
     invalid_user = validate_json_string(invalid_json)
     print(invalid_user)
 except ValueError as e:
-    print("Validierungsfehler:", e)
+    print("Validation error:", e)
